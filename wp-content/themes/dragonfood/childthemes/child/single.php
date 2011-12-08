cce3fafc7824b5e5bc75e3ad8e7491637f63f880
Seth Jeremi Malaki <electricjesus@quadsrv64.com>
2011-12-08 17:48:01 +0800

    Some changes for single blog post (not completely fixed tho) and some optimizations for cache and header handling

--- wp-content/themes/dragonfood/childthemes/child/single.php
+++ wp-content/themes/dragonfood/childthemes/child/single.php
@@ -1,23 +1,57 @@
 <?php WpMvc::app()->view->render('header');?>
+<? /*
 <div class="main-col span-15">
     <h1><?php echo $thePost->title;?></h1>
-    <div><?php echo $thePost->content;?></div>
+    <div><?= the_content(); ?></div>
 
     <?php if($thePost->type==='post'):?>
     <div class="post-meta align-right">
         <?php if(count($thePost->tags) > 0): ?>
             <p>Tag: </p>
         <?php endif; ?>
-        <p>Posted in : <?php print_r($thePost->categories);?>
-        
-        </p>
+        <p>Posted in : <?php print_r($thePost->categories);?></p>
     </div>
     <?php endif;?>
-     <?php WpMvc::app()->view->render('widgets/widgetShare')?>
+    <?php WpMvc::app()->view->render('widgets/widgetShare')?>
    
     <?php WpMvc::app()->view->render('comments',array('thePost'=>$thePost));?>
 </div>
-<div class="side-col span-8 push-1 last">
+ */ ?>
+<div class="blogcontainer">
+	<div class="blogwrapper  blog-post-1">
+		<div class="blogimage" style="overflow:hidden;">
+			<a href="<?php the_permalink(); ?>"><img src="<?= get_featured_image(); ?>" alt="<?php the_title(); ?>" /></a>
+		</div>
+		<div class="blogheader">
+			<a href="<?php the_permalink(); ?>"><div class="blogtitle"><?php the_title(); ?></div></a>
+			<div class="blogdate"><?= get_the_date() ?></span> | <a class="author"><?= get_the_author(); ?></div>
+		</div>
+		<div class="blogbody">
+			<?php global $more; $more = 1; ?>
+			<?=	 the_content();?>
+			<img src="/wp-content/themes/dragonfood/images/divider.png" class="divider" height="1" width="550"/>
+			<?php if($thePost->type==='post'):?>
+			<div class="post-meta post-tags">
+				<?php if(count($thePost->tags) > 0): ?>
+					<p>Tag: </p>
+				<?php endif; ?>
+				<p>Posted in : <?php 
+				foreach($thePost->categories as $c){
+					echo $c;
+					$cat = get_category( $c );
+					print_r($cat);
+					$cats[] = array( 'name' => $cat->name, 'slug' => $cat->slug );
+				}
+				?></p>
+			</div>
+			<?php endif; ?>
+			<div class="comments">
+				<?php WpMvc::app()->view->render('comments',array('thePost'=>$thePost));?>
+			</div>
+		</div>
+	</div>
+</div>
+<div id="widgetwrapper">
     <div class="widget share">
         <h3>Do you like this?</h3>
         <span class="twitter-share">
@@ -26,15 +60,9 @@
         </span>
         &nbsp;&nbsp;
         <span class="fb-share">
-            <iframe src="http://www.facebook.com/plugins/like.php?href=<?php    the_permalink();?>&amp;layout=button_count&amp;show_faces=false&amp;width=150&amp;action=like&amp;colorscheme=light&amp;height=21" scrolling="no" frameborder="0" style="border:none; overflow:hidden; width:200px; height:21px; color:#FFF;" allowTransparency="true"></iframe>
+            <iframe src="http://www.facebook.com/plugins/like.php?href=<?php the_permalink();?>&amp;layout=button_count&amp;show_faces=false&amp;width=150&amp;action=like&amp;colorscheme=light&amp;height=21" scrolling="no" frameborder="0" style="border:none; overflow:hidden; width:200px; height:21px; color:#FFF;" allowTransparency="true"></iframe>
         </span>
-        
-        
     </div>
-    
-    <?php WpMvc::app()->view->render('widgets/widgetAuthor',compact('thePost'));?>
-    <?php WpMvc::app()->view->render('widgets/widgetConnect');?>
     <?php dynamic_sidebar('ContentRight');?>
-    <?php WpMvc::app()->view->render('widgets/widgetRelatedPost',  compact('thePost'));?>
 </div>
 <?php WpMvc::app()->view->render('footer');?>
